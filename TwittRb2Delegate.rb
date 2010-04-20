5c795c51c9e8d0a5ff4d421cef7a48d6f670222e
Patrick Huesler <patrick.huesler@gmail.com>
2010-04-20 14:28:48 +0200

    Initial commit

---
+++ TwittRb2Delegate.rb
@@ -0,0 +1,72 @@
+#
+#  TwittRb2Delegate.rb
+#  TwittRb2
+#
+#  Created by phuesler on 20.04.10.
+#  Copyright (c) 2010 huesler informatik. All rights reserved.
+#
+
+class TwittRb2Delegate
+  attr_accessor :credentialsWindow, :mainWindow
+  attr_accessor :usernameField, :passwordField
+  attr_accessor :username, :password
+  attr_accessor :tableView, :statusLabel
+  attr_accessor :updates
+  attr_accessor :reloadButton
+  
+  def initialize
+    @timeline = []
+  end
+  
+  def applicationDidFinishLaunching(notification)
+    NSApp.beginSheet(credentialsWindow,
+                    modalForWindow:mainWindow,
+                    modalDelegate:nil,
+                    didEndSelector:nil,
+                    contextInfo:nil)
+    @twitterEngine = MGTwitterEngine.alloc.initWithDelegate(self)
+  end
+  
+  def submitCredentials(sender)
+    self.username = usernameField.stringValue
+    self.password = passwordField.stringValue
+    hideCredentialsWindow(sender)
+    reload(self)
+  end
+  
+  def cancelCredentials(sender)
+    hideCredentialsWindow(sender)
+  end
+  
+  def reload(sender)
+    NSLog "Reloading"
+    @twitterEngine.setUsername(username, password:password)
+    @twitterEngine.getFollowedTimelineSinceID(0, startingAtPage:0, count:20)
+  end
+  
+  def statusesReceived(statuses, forRequest:identifier)
+     @timeline = []
+     statuses.each do |status|
+       @timeline << {user: status["user"]["name"],  tweet: status["text"]}
+     end
+     self.tableView.reloadData
+  end
+  
+  def numberOfRowsInTableView(tableView)
+    @timeline.size
+  end
+  
+  def tableView(tableView, objectValueForTableColumn: column, row: row)
+    if row < @timeline.size - 1
+      return @timeline[row].valueForKey(column.identifier.to_sym)
+    end
+    return nil
+  end
+  
+  private
+  
+  def hideCredentialsWindow(sender)
+    NSApp.endSheet(credentialsWindow)
+    credentialsWindow.orderOut(sender)
+  end
+end
